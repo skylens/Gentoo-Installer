d477018b23459a3089643a2313eee1bedc584a8b
skylens <skylens116@outlook.com>
2017-10-13 10:33:05 +0800

    VMware fro Linux

--- test1.sh
+++ test1.sh
@@ -1,15 +1,21 @@
 #! /bin/bash
 # http://archives.seul.org/seul/project/Feb-1998/msg00069.html
-function test()
+function checkuse()
 {
+	[ $(id -u) != "0" ] && { whiptail --title "错误" --backtitle "Gentoo 安装程序" --msgbox "需要 root 用户权限，是否退出？" 10 60; exit 1; }
 }
-
-if (whiptail --title "Gentoo 安装程序" --backtitle "感谢使用" --yesno "安装前请先备份数据"  10 40)
+#checkuse
+function start_install()
+{
+	whiptail --title "Gentoo 安装程序" --backtitle "Gentoo 安装程序" --yes-button "安装" --no-button "退出" --yesno "欢迎使用 Gentoo TUI 安装程序"  10 35
+}
+if (start_install)
 then
-        echo -e "\n继续!\n"
-elif(whiptail --title "Gentoo 安装程序" --backtitle "感谢使用" --yesno "确定要退出?" 7 40)
+	whiptail --title "分区" --backtitle "Gentoo 安装程序" --yes-button "继续" --no-button "退出" --yesno "你磁盘的数据已经备份好了？该不操作将会格式化你的磁盘！" 10 40
+	
+elif(whiptail --title "Gentoo 安装程序" --backtitle "Gentoo 安装程序" --yesno "确定要退出?" 7 40)
 then
-        echo -e "\n是的!\n"
+        start_install
 else
-        echo -e "\n不退出\n"
+        exit 1;
 fi
